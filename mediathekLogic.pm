e6333911e84299a15912d47f2e15763b15d0eb65
Stefan Böhringer <github@s-boehringer.org>
2014-01-14 20:43:11 +0100

    top list + random list fetching

--- mediathekLogic.pm
+++ mediathekLogic.pm
@@ -117,7 +117,7 @@ class My::Schema {
 			$self->updateWithXml($c, $xml);
 		} else {
 			my @serverList = $self->serverList($c);
-			$self->updateWithXml($c, main::meta_get([$serverList[$i]], "$c->{location}/database_raw.xml_$i.bz2",
+			$self->updateWithXml($c, main::meta_get([$serverList[$i]], "$c->{location}/database_raw.xml.bz2",
 					refetchAfter => $c->{refreshTvitems}, seq => 1));
 			for (my $i = 0; $i < $c->{refreshServersCount}; $i++) {
 				$xml = main::meta_get([@serverList], "$c->{location}/database_raw.xml_$i.bz2",
