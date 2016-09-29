9a489e740a06f8380f4fe8a7698313d55df98562
Stefan Boehringer <github@s-boehringer.org>
2016-09-29 23:21:25 +0200

    - fetch-parse-youtube.pl finished - schema update

--- fetch-parse-youtube.pl
+++ fetch-parse-youtube.pl
@@ -3,6 +3,7 @@
 #Sat Sep 24 22:30:07 CEST 2016
 
 #./fetch-parse-youtube.pl --fetch https://www.youtube.com/channel/UCKQ_j0fm3NioMRE3i-1CryA
+#youtube-dl -j --get-title --flat-playlist https://www.youtube.com/channel/UCKQ_j0fm3NioMRE3i-1CryA
 
 use TempFileNames;
 use Set;
@@ -13,7 +14,8 @@ use Data::Dumper;
 $main::d = { triggerPrefix => '', sep => ':<>:', parse => '-',
 	cache => firstDef($ENV{MEDIATHEK_YOUTUBE_CACHE},
 		"$ENV{HOME}/.local/share/applications/mediathek/youtube-"),
-	minOverlap => 1
+	minOverlap => 1,
+	Nmax => 3
 };
 # options
 $main::o = [ '+fetch=s', 'sep=s' ];
@@ -29,11 +31,38 @@ HELP_TEXT
 
 my $stringREraw='(?:(?:[_\/\-a-zA-Z0-9.]+)|(?:\"(?:(?:\\\\.)*(?:[^"\\\\]+(?:\\\\.)*)*)\"))';
 my $stringRE='(?:([_\/\-a-zA-Z0-9.]+)|(?:\"((?:\\\\.)*(?:[^"\\\\]+(?:\\\\.)*)*)\"))';
+my $keyValueRE = "$stringRE\s*:\s*$stringRE";
 sub dequoteBackslash { my ($str) = @_; $str =~ s/\\(.)/$1/g; return $str; }
 sub extractString {
 	dequoteBackslash(join('', $_[0] =~ m{$stringRE}so))
 }
 sub Chop { substr($_[0], 0, -1) }
+
+my @colSel = ('id', 'channel', 'title' );
+sub fetch { my ($o) = @_;
+	my $url = $o->{fetch};
+	my $channel = splitPathDict($url)->{file};
+	my $cmd = "youtube-dl -j --get-title --flat-playlist $url";
+	my $r = System($cmd, 2, undef, { returnStdout => 'YES' } );
+	my @lines = split("\n", $r->{output});
+
+	foreach my $l (@lines) {
+		my %el = (grep { $_ ne '' } ($l =~ m[$stringRE]sg), channel => $channel);
+		#print(Dumper(\%el));
+		print join($o->{sep}, @el{@colSel}), "\n";
+	}
+}
+
+#main $#ARGV @ARGV %ENV
+	#initLog(2);
+	my $c = StartStandardScript($main::d, $main::o);
+exit(0);
+
+#
+#	<p> attic
+#
+
+__DATA__
 
 sub fetch { my ($o) = @_;
 	my $url = $o->{fetch};
@@ -44,14 +73,15 @@ sub fetch { my ($o) = @_;
 
 	# <p> read cache
 	my @data;
+	my %cache;
 	my $overlap = 0;	# count of urls seen before
-	my %urls;
-	if (-e $o->{cache}) {
-		my $csv = readCsv($o->{cache});
+	my $cachePath = $o->{cache}. splitPathDict($o->{fetch})->{file};
+	Log("Cache path: $cachePath", 2);
+	if (-e $cachePath) {
+		my $csv = readCsv($cachePath);
 		@data = map { makeHash($csv->{factors}, $_) } @{$csv->{data}};
 		%cache = %{dictWithKeys([map { $_->{title} } @data])};
 	}
-	print(Dumper(\%cache));
 
 	# <p> iterate list (newest -> oldest by assumption)
 	for (my $i = 0; my $t = $fh->getline; $i++) {
@@ -66,14 +96,10 @@ sub fetch { my ($o) = @_;
 			last if ($overlap >= $o->{minOverlap});
 		} else {
 			push(@data, $el);
+			print join($o->{sep}, @{$el}{'title', 'url'}), "\n";
 		}
+		last if ($i + 1 >= $o->{Nmax});
 	}
 	$fh->close();
-	print(Dumper([@data]));
-	writeCsv({ data => [@data] }, $o->{cache});
+	writeCsv({ data => [@data] }, $cachePath);
 }
-
-#main $#ARGV @ARGV %ENV
-	#initLog(2);
-	my $c = StartStandardScript($main::d, $main::o);
-exit(0);
